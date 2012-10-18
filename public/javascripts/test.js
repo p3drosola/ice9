1150aa8211504e8bfa509c37aa657f6834569ce8
Pedro Sola <p3dro.sola@gmail.com>
2012-10-18 18:01:45 +0200

    playing with cryptoJS

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,6 +1,24 @@
 
+var clean =  CryptoJS.enc.Utf8.parse("Message");
+var encrypted = CryptoJS.AES.encrypt(clean, "password");
 
 
-var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+console.log('key:'+ encrypted.key);
+console.log('iv:'+ encrypted.iv);
 
-//var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+var encrypted_array = encrypted.ciphertext.words; // byte arrray here. this goes over the wire
+
+// decryption
+var encrypted_words = CryptoJS.lib.WordArray.create(encrypted_array);
+var cypherParams = CryptoJS.lib.CipherParams.create({
+  ciphertext: encrypted_words
+});
+
+
+var decrypted1 = CryptoJS.AES.decrypt(encrypted, "password"); // using passphrase
+var decrypted2 = CryptoJS.AES.decrypt(cypherParams, encrypted.key, {iv:  encrypted.iv}); // using key + iv
+
+var string1 = CryptoJS.enc.Utf8.stringify(decrypted1);
+var string2 = CryptoJS.enc.Utf8.stringify(decrypted2);
+
+console.log('decrypted:', string2);
